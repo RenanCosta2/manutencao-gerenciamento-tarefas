b2aa6e17152380ac0cc76e34511884619c61a7a0
RenanCosta2 <renanleitedacosta@gmail.com>
2024-12-12 13:47:55 -0300

    feat: adição de exceções | modificações para clean code

--- tarefas/api/views.py
+++ tarefas/api/views.py
@@ -1,41 +1,80 @@
-from rest_framework import viewsets
-from tarefas.models import TarefaModel
-from tarefas.api.serializers import TarefaSerializer
+"""
+Importações do Django REST framework e das Tarefas
+"""
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import PermissionDenied, NotAuthenticated
+from tarefas.models import TarefaModel
+from tarefas.api.serializers import TarefaSerializer
+
 
 class TarefaViewSet(ModelViewSet):
+    """
+    ViewSet para gerenciamento de Tarefas
+    """
+
     serializer_class = TarefaSerializer
     permission_classes = [AllowAny]
     queryset = TarefaModel.objects.all()
 
-    def create(self, request):
-        serializer = TarefaSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
+    def create(self, request, *args, **kwargs):
 
-        hora_inicio = serializer.validated_data['hora_inicio']
-        hora_fim = serializer.validated_data['hora_fim']
+        try:
+            serializer = TarefaSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
 
-        # Verificar se o horário da nova tarefa já está ocupado
-        in_database = TarefaModel.objects.filter(
-            hora_inicio__lt=hora_fim,  # Conflito se o horário de início de alguma tarefa for antes de a nova terminar
-            hora_fim__gt=hora_inicio   # Conflito se o horário de fim de alguma tarefa for depois de a nova começar
-        ).exists()
+            hora_inicio = serializer.validated_data['hora_inicio']
+            hora_fim = serializer.validated_data['hora_fim']
 
-        if not in_database:
-            
-            nova_tarefa = TarefaModel(
-                titulo=serializer.validated_data['titulo'],
-                descricao=serializer.validated_data['descricao'],
-                hora_inicio=serializer.validated_data['hora_inicio'],
-                hora_fim=serializer.validated_data['hora_fim'],
-            )
-            nova_tarefa.save()
+            # Verificar se o horário da nova tarefa já está ocupado
+            in_database = TarefaModel.objects.filter(
+                # Conflito se o horário de início de alguma tarefa
+                # for antes de a nova terminar
+                hora_inicio__lt=hora_fim,
+                # Conflito se o horário de fim de alguma tarefa
+                # for depois de a nova começar
+                hora_fim__gt=hora_inicio
+            ).exists()
 
-            
-            serializer_saida = TarefaSerializer(nova_tarefa)
-            return Response({"Info": "Tarefa criada!", "data": serializer_saida.data}, status=status.HTTP_201_CREATED)
-        else:
-            return Response({"Info": "Horário indisponível"}, status=status.HTTP_409_CONFLICT)
+            if not in_database:
+
+                nova_tarefa = TarefaModel(
+                    titulo=serializer.validated_data['titulo'],
+                    descricao=serializer.validated_data['descricao'],
+                    hora_inicio=serializer.validated_data['hora_inicio'],
+                    hora_fim=serializer.validated_data['hora_fim'],
+                )
+                nova_tarefa.save()
+
+                serializer_saida = TarefaSerializer(nova_tarefa)
+                return Response(
+                    {"Info": "Tarefa criada!", "data": serializer_saida.data},
+                    status=status.HTTP_201_CREATED
+                    )
+
+            return Response(
+                {"Info": "Horário indisponível"},
+                status=status.HTTP_409_CONFLICT
+                )
+        except ValueError:
+            return Response(
+                {"Erro": "Dados inválidos!"},
+                status=status.HTTP_409_CONFLICT
+                )
+        except KeyError:
+            return Response(
+                {"Erro": "Algum dado faltando ou errado."},
+                status=status.HTTP_400_BAD_REQUEST
+                )
+        except PermissionDenied:
+            return Response(
+                {"Erro": "Você não possui permissão para isso."},
+                status=status.HTTP_403_FORBIDDEN
+                )
+        except NotAuthenticated:
+            return Response(
+                {"Erro": "Você não está autenticado."},
+                status=status.HTTP_401_UNAUTHORIZED
+                )
