7feed3027c2a951e0d26fabcba231d5393019274
RenanCosta2 <renanleitedacosta@gmail.com>
2024-12-17 10:43:23 -0300

    feat: teste inicial do create de tarefa

--- tarefas/tests.py
+++ tarefas/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from .models import TarefaModel
+from django.utils import timezone
+from datetime import timedelta
 
-# Create your tests here.
+class TarefaModelTest(TestCase):
+    def test_create_tarefa(self):
+        """
+        Testa a criação de uma TarefaModel
+        """
+        
+        titulo = "Tarefa de Teste"
+        descricao = "Descrição da tarefa de teste."
+        hora_inicio = timezone.now()
+        hora_fim = hora_inicio + timedelta(hours=2)
+
+        tarefa = TarefaModel.objects.create(
+            titulo=titulo,
+            descricao=descricao,
+            hora_inicio=hora_inicio,
+            hora_fim=hora_fim
+        )
+
+        # Recupera do banco
+        tarefa_salva = TarefaModel.objects.get(id=tarefa.id)
+
+        # Asserções para validar os campos
+        self.assertEqual(tarefa_salva.titulo, titulo)
