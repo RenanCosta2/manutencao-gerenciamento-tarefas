96b532d250fa1cff4d4949c419cc2b4874ca3fe8
RenanCosta2 <renanleitedacosta@gmail.com>
2025-01-06 11:44:48 -0300

    fix: ajustes no service de tarefa

--- tarefas/services.py
+++ tarefas/services.py
@@ -1,27 +1,29 @@
 from .models import TarefaModel
 
-def create(data):
-    hora_inicio = data['hora_inicio']
-    hora_fim = data['hora_fim']
+class TarefaService:
 
-    # Verificar se o horário da nova tarefa já está ocupado
-    in_database = TarefaModel.objects.filter(
-        # Conflito se o horário de início de alguma tarefa
-        # for antes de a nova terminar
-        hora_inicio__lt=hora_fim,
-        # Conflito se o horário de fim de alguma tarefa
-        # for depois de a nova começar
-        hora_fim__gt=hora_inicio
-    ).exists()
+    def create(self, data):
+        hora_inicio = data['hora_inicio']
+        hora_fim = data['hora_fim']
 
-    if not in_database:
+        # Verificar se o horário da nova tarefa já está ocupado
+        in_database = TarefaModel.objects.filter(
+            # Conflito se o horário de início de alguma tarefa
+            # for antes de a nova terminar
+            hora_inicio__lt=hora_fim,
+            # Conflito se o horário de fim de alguma tarefa
+            # for depois de a nova começar
+            hora_fim__gt=hora_inicio
+        ).exists()
 
-        nova_tarefa = TarefaModel(
-            titulo=data['titulo'],
-            descricao=data['descricao'],
-            hora_inicio=data['hora_inicio'],
-            hora_fim=data['hora_fim'],
-        )
-        nova_tarefa.save()
+        if not in_database:
 
-        return nova_tarefa
+            nova_tarefa = TarefaModel(
+                titulo=data['titulo'],
+                descricao=data['descricao'],
+                hora_inicio=data['hora_inicio'],
+                hora_fim=data['hora_fim'],
+            )
+            nova_tarefa.save()
+
+            return nova_tarefa
